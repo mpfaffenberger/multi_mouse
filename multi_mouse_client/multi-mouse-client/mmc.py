d7d30c006b5ca9eaca3b756ea7d07fed515be2f5
Mike <Mike>
2021-05-11 22:06:44 -0400

    patching big sur opengl

--- multi_mouse_client/multi-mouse-client/mmc.py
+++ multi_mouse_client/multi-mouse-client/mmc.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 import glfw
-import OpenGL.GL as gl
+
+try:
+    import OpenGL as ogl
+    try:
+        import OpenGL.GL as gl  # this fails in <=2020 versions of Python on OS X 11.x
+    except ImportError:
+        print('Drat, patching for Big Sur')
+        from ctypes import util
+        orig_util_find_library = util.find_library
+        def new_util_find_library( name ):
+            res = orig_util_find_library( name )
+            if res: return res
+            return '/System/Library/Frameworks/'+name+'.framework/'+name
+        util.find_library = new_util_find_library
+except ImportError:
+    pass
+
 import socket
 import imgui
 import uuid
